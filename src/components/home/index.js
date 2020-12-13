c1cf1da4e7c5d4e03d220100569c456bbc059dd1
John Shearer <john@ultranaut.com>
2020-12-13 18:38:03 -0500

    Slight css refactor, use css module

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Home.module.css';
 
 const Home = () => {
   return (
     <div className="home">
-      <header>
-        <h1>ultranaut</h1>
+      <header className={styles.header}>
+        <h1 className={styles.title}>ultranaut</h1>
       </header>
     </div>
   );
