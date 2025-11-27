f439ec55e88681e063c9b0ffe1141cf63f8e0720
John Shearer <440100+ultranaut@users.noreply.github.com>
2025-11-27 11:08:33 -0500

    Randomize backgroundImage ;

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styles from './Home.module.css';
+import bee from '../bee.jpg';
+import porchFlowers from '../porch-flowers.jpg';
+import starField from '../Star-field-hubble-deep.jpg';
+
+
+const images =[bee, porchFlowers, starField];
+const randomImage = images[Math.floor(Math.random() * images.length)];
 
 const Home = () => {
   return (
-    <div className="home">
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${randomImage})`
+      }}>
       <header className={styles.header}>
         <h1 className={styles.title}>ultranaut</h1>
       </header>
